fd871e67329c1ff537a074e48d17a57b7055d713
soph-Perez <sophiaperez040922@gmail.com>
2025-10-04 17:03:13 -0600

    added styling for user signup

--- app/usignup/page.tsx
+++ app/usignup/page.tsx
@@ -1,13 +1,12 @@
-export default function UserSignUpPage() {
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import UserSignUp from "@/components/usersignup";
+export default function SignUp() {
   return (
-    <div>
-      <h1>User Signup</h1>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <>
+      <Header />
+      <UserSignUp />
+      <Footer />
+    </>
   );
 }
--- app/uview/page.tsx
+++ app/uview/page.tsx
@@ -2,6 +2,11 @@ export default function UserView() {
   return (
     <div>
       <h1>USER VIEW</h1>
+
+      <div>
+        <input type="text" placeholder="Enter your location" />
+        <button type="button">Search</button>
+      </div>
     </div>
   );
 }

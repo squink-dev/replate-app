498ed3862b172bd3ba7f4054776aaf8286b7454c
squink-dev <squireink13@gmail.com>
2025-10-05 06:52:10 -0600

    fix vercel deployment

--- app/user/signup/page.tsx
+++ app/user/signup/page.tsx
@@ -1,11 +1,21 @@
+import { Suspense } from "react";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import UserSignUp from "@/components/usersignup";
+
 export default function SignUp() {
   return (
     <>
       <Header />
-      <UserSignUp />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-h-screen">
+            Loading...
+          </div>
+        }
+      >
+        <UserSignUp />
+      </Suspense>
       <Footer />
     </>
   );
